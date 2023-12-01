30c11c299dd84ec0a7d8091c38ec5d35d508f547
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-12-01 15:29:33 +0330

    5-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//modules
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
